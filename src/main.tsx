1be7f6d593bf8fcf07f4788bb3bb3c8cf4bf208d
Amadeo Velez <amadeo.velez@gmail.com>
2023-08-29 18:28:00 -0400

    moved unused import

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,14 @@
-import React from 'react'
 import App from './App.tsx'
 import './index.css'
 
 //Swap import below to switch to React 18
-/* import ReactDOM from 'react-dom/client' */
+/* import ReactDOM from 'react-dom/client'
+import React from 'react'
+*/
 import ReactDOM from 'react-dom'
 
 
-//Swap Render method below to switch to React 18
+//Swap Render method below to switch to REACT 18
 
 /*
 ReactDOM.createRoot(document.getElementById('root')!).render(
